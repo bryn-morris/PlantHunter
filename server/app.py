304e3a1ed684651d0ea26374ee42c86abc666b3e
Brynfor Morris <brynfor.morris@gmail.com>
2023-04-25 08:09:45 -0500

    bugfix for special characters

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Observations, Plants
+from models import User, Observation, Plant
 
 # Views go here!
 
