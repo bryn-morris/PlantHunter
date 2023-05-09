6bc02807a21b2d40a4dd489917059586b003cad7
Brynfor Morris <brynfor.morris@gmail.com>
2023-05-08 21:48:44 -0500

    minor refactor to tab bar to avoid depreccated element

--- client/components/AppContainer.js
+++ client/components/AppContainer.js
@@ -3,14 +3,11 @@ import IndexStack from "./IndexStack/IndexStack";
 import Compendium from "./Compendium.js";
 import ProfileStack from "./ProfileStack/ProfileStack";
 import { PlantProvider } from "../context/PlantContext";
-import { Ionicons, Entypo, AntDesign } from '@expo/vector-icons';
-import { useState } from "react";
+import { Ionicons, Entypo } from '@expo/vector-icons';
 
 function AppContainer() {
 
     const Tab = createBottomTabNavigator()
-
-    const [isProfileColor, setIsProfileColor ] = useState(false)
 
     ////////////////////////////////////////////////
     ///////  Render On This Page
@@ -18,32 +15,39 @@ function AppContainer() {
 
     return(
         <PlantProvider>
-            <Tab.Navigator 
-                tabBarOptions={{
-                    tabBarActiveTintColor: '#4a7c59',
-                    tabBarInactiveTintColor: '#4e372c',
-                    tabBarIconSize: 24,
-                }}
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                tabBarActiveTintColor: '#4a7c59',
+                tabBarInactiveTintColor: '#4e372c',
+                tabBarIconSize: 28,
+                tabBarActiveBackgroundColor: '#d5ceae',
+                tabBarInactiveBackgroundColor: '#d5ceae',
+                tabBarLabelStyle: {
+                    fontWeight: 'bold',
+                    fontSize: 14,
+                    color: route.state && route.state.index === 0 ? '#4a7c59' : '#4e372c',
+                },
+                tabBarIcon: ({ size, focused }) => {
+                    let iconName;
+
+                    if (route.name === 'My Profile') {
+                        return (<Ionicons name="md-leaf" size={size} 
+                            color={focused ? "#4a7c59": "#4e372c"}/>)
+                    } else if (route.name === 'My Index') {
+                        iconName = 'flower';
+                    } else if (route.name === 'My Compendium') {
+                        iconName = 'open-book';
+                    }
+
+                    return <Entypo name={iconName} size={size} color={focused ? '#4a7c59' : '#4e372c'} />;
+                },
+                })}
             >
                 <Tab.Screen 
                     name = 'My Profile' 
                     component={ProfileStack}
                     options={{ 
                         headerShown: false,
-                        tabBarActiveBackgroundColor: "#d5ceae",
-                        tabBarInactiveBackgroundColor: "#d5ceae",
-                        tabBarIcon: ({ color, size, focused }) => (
-                            <Ionicons
-                                name="md-leaf"
-                                size={size}
-                                color={focused ? "#4a7c59": "#4e372c"}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontWeight: "bold",
-                            fontSize: 14,
-                            color: ({ focused }) => (focused ? '#4a7c59' : '#4e372c'),
-                        }
                      }}
                      
                 />
@@ -52,40 +56,12 @@ function AppContainer() {
                     component={IndexStack}
                     options={{ 
                         headerShown: false,
-                        tabBarActiveBackgroundColor: "#d5ceae",
-                        tabBarInactiveBackgroundColor: "#d5ceae",
-                        tabBarIcon: ({ color, size, focused }) => (
-                            <Entypo 
-                                name="flower"
-                                size={size}
-                                color={focused ? "#4a7c59": "#4e372c"}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontWeight: "bold",
-                            fontSize: 14,
-                            color: ({ focused }) => (focused ? '#4a7c59' : '#4e372c'),
-                        }
                      }}  
                 />
                 <Tab.Screen 
                     name = 'My Compendium' 
                     component={Compendium}
                     options={{ 
-                        tabBarActiveBackgroundColor: "#d5ceae",
-                        tabBarInactiveBackgroundColor: "#d5ceae",
-                        tabBarIcon: ({ color, size, focused }) => (
-                            <AntDesign 
-                                name="aliwangwang"
-                                size={size}
-                                color={focused ? "#4a7c59": "#4e372c"}
-                            />
-                        ),
-                        tabBarLabelStyle: {
-                            fontWeight: "bold",
-                            fontSize: 14,
-                            color: ({ focused }) => (focused ? '#4a7c59' : '#4e372c'),
-                        }
                      }}   
                 />
             </Tab.Navigator>  
